46a9375dc56fc3f9bf0a3212b895e6d04bf18d1f
sakurartro <imsusimgg@gmail.com>
2026-09-05 17:45:22 +0700

    config

--- TMessagesProj/src/main/java/org/telegram/messenger/SpotifyApiConfig.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/SpotifyApiConfig.java
@@ -2,9 +2,9 @@ package org.telegram.messenger;
 
 /** Put your server endpoints here. Empty control URLs disable that action. */
 public final class SpotifyApiConfig {
-    public static final String STATUS_URL = "";
-    public static final String PLAY_URL = "";
-    public static final String PAUSE_URL = "";
+    public static final String STATUS_URL = "http://10.248.199.130:8000/spotify/current";
+    public static final String PLAY_URL = "http://10.248.199.130:8000/spotify/current";
+    public static final String PAUSE_URL = "http://10.248.199.130:8000/spotify/current";
     public static final String NEXT_URL = "";
     public static final String PREVIOUS_URL = "";
 
